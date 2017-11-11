ae29bcdbae24e128a8c9b0c5d6222c5a12813270
Joanna <jmgsese@gmail.com>
2017-11-11 15:14:17 -0600

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to '/'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :password,
+      :height,
+      :tickets,
+      :happiness,
+      :nausea,
+      :admin
+    )
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
-  get '/', to: 'user#index'
+  get '/', to: 'users#index'
+  get '/signin', to: 'sessions#new'
+  get '/home', to: 'users#show'
 
+  resources :users
+  resources :sessions
 
+  root to: 'users#index'
 end
